18f51b66b0d0c07aeea849adb98fdd746aab78fc
Mohammed Alosaimi <mohammedalosaimi@gmail.com>
2021-06-26 22:53:50 +0300

    php file to give json file about run angles info

---
+++ back-end/runAnglesInfo.php
@@ -0,0 +1,21 @@
+<?php
+
+//this php file to chech give josn file whiter the arm is on or off
+
+include_once "config.php";
+
+$sql = "SELECT on_off FROM run WHERE id = 1";
+
+$result = mysqli_query($conn,$sql);
+
+$count = mysqli_num_rows($result);
+
+$v = 0;
+
+
+while($row = mysqli_fetch_assoc($result)){
+    $v = $row['on_off'];
+}
+
+echo json_encode($v);
+?>
